0921b7735850c3d673ff2f7e088925eb0cce5f52
William Gao <gaowhui@gmail.com>
2018-02-02 23:23:14 +0000

    working out mailer issues

---
+++ config/initializers/setup_mailer.rb
@@ -0,0 +1,13 @@
+if Rails.env.development? || Rails.env.production?
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'heroku.com',
+  #'https://hblockpedia.herokuapp.com',
+  user_name:  ENV["GMAIL_USERNAME"],
+  password:  ENV["GMAIL_PASSWORD"],
+  authentication:       'plain',
+  enable_starttls_auto: true 
+}
+end
